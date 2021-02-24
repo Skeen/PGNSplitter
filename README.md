10cc2c6af7a65e3b868f68d07ed3bb828168dfb3
Emil Madsen <sovende@gmail.com>
2021-02-24 21:43:15 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,74 @@
 # PGNSplitter
-Streaming PGN splitter
+Streaming PGN splitter.
+
+Traditional PGN databases, like SCID or ChessBase, fail to open large PGN files.
+Thus it can be helpful to split the PGN files into smaller files.
+
+## Installation
+Establish a virtual environment, and install the requirements:
+```
+pip install -r requirements.txt
+```
+Then run the program as `python main.py`.
+
+## Usage
+```
+Usage: main.py [OPTIONS] FILENAME OUTPUT_FOLDER
+
+  Streaming PGN splitter.
+
+  Reads FILENAME and splits it into seperate PGN files output to
+  OUTPUT_FOLDER. Output files are named 0.pgn, 1.pgn, ..., n.pgn.
+
+  By default all games are exported, but the number of games being exported
+  can be limited using --games, and the scanning can be offset using
+  --start.
+
+Arguments:
+  FILENAME       Input file to be split into seperate PGN files.  [required]
+  OUTPUT_FOLDER  Output folder for PGN files.  [required]
+
+Options:
+  --skip INTEGER RANGE            Number of games to skip from FILENAME before
+                                  producing output.
+
+  --games INTEGER RANGE           Number of games to extract from FILENAME.
+  --dry-run / --no-dry-run        Dry run the operation, thereby not producing
+                                  any files.  [default: False]
+
+  --install-completion [bash|zsh|fish|powershell|pwsh]
+                                  Install completion for the specified shell.
+  --show-completion [bash|zsh|fish|powershell|pwsh]
+                                  Show completion for the specified shell, to
+                                  copy it or customize the installation.
+
+  --help                          Show this message and exit.
+```
+
+## Example
+```
+$ wget https://database.lichess.org/standard/lichess_db_standard_rated_2017-04.pgn.bz2
+--2021-02-24 21:37:34--  https://database.lichess.org/standard/lichess_db_standard_rated_2017-04.pgn.bz2
+Resolving database.lichess.org (database.lichess.org)... 147.135.255.84, 2001:41d0:303:2e54::
+Connecting to database.lichess.org (database.lichess.org)|147.135.255.84|:443... connected.
+HTTP request sent, awaiting response... 200 OK
+Length: 2946587091 (2.7G) [application/octet-stream]
+...
+
+$ bunzip2 lichess_db_standard_rated_2017-04.pgn.bz2
+
+$ mkdir output
+
+$ python main.py lichess_db_standard_rated_2017-04.pgn output/
+  5%|█▊                                | 1.30G/24.2G [00:19<05:48, 65.7Mbytes/s]
+...
+
+$ ls -v output
+0.pgn
+1.pgn
+2.pgn
+3.pgn
+4.pgn
+...
+
+```
---
+++ main.py
@@ -0,0 +1,102 @@
+from itertools import islice
+from os.path import getsize
+from pathlib import Path
+from typing import Iterator, Optional
+
+import typer
+from chess.pgn import skip_game
+from more_itertools import consume, pairwise
+from tqdm import tqdm
+
+
+def find_byte_boundaries(filename: Path) -> Iterator[int]:
+    """Generator of byte offsets for the start and end of each PGN."""
+    yield 0
+    with open(filename, "r") as pgn:
+        while skip_game(pgn):
+            yield pgn.tell()
+        yield pgn.tell()
+
+
+def _tqdm_boundaries(filename: Path, it: Iterator[int]) -> Iterator[int]:
+    """Apply tqdm to the provided iterator."""
+    byte_length: int = getsize(filename)
+    pbar = tqdm(total=byte_length, unit="bytes", unit_scale=True)
+
+    for new_position in it:
+        # pbar.update expects a relative update, not an absolute
+        difference: int = new_position - pbar.n
+        pbar.update(difference)
+
+        yield new_position
+
+
+def split_file(filename: Path, seperators: Iterator[int]) -> Iterator[bytes]:
+    """Generator of bytes, blocks correspond to PGN."""
+    with open(filename, "rb") as pgn:
+        for start, end in pairwise(seperators):
+            difference: int = end - start
+            pgn.seek(start)
+            yield pgn.read(difference)
+
+
+def split(
+    skip: Optional[int] = typer.Option(
+        default=None,
+        min=0,
+        help="Number of games to skip from FILENAME before producing output.",
+    ),
+    games: Optional[int] = typer.Option(
+        default=None, min=0, help="Number of games to extract from FILENAME."
+    ),
+    filename: Path = typer.Argument(
+        ...,
+        exists=True,
+        file_okay=True,
+        dir_okay=False,
+        readable=True,
+        help="Input file to be split into seperate PGN files.",
+    ),
+    output_folder: Path = typer.Argument(
+        ...,
+        exists=True,
+        file_okay=False,
+        dir_okay=True,
+        writable=True,
+        help="Output folder for PGN files.",
+    ),
+    dry_run: bool = typer.Option(
+        default=False, help="Dry run the operation, thereby not producing any files."
+    ),
+):
+    """Streaming PGN splitter.
+
+    Reads FILENAME and splits it into seperate PGN files output to OUTPUT_FOLDER.
+    Output files are named 0.pgn, 1.pgn, ..., n.pgn.
+
+    By default all games are exported, but the number of games being exported can
+    be limited using --games, and the scanning can be offset using --start.
+    """
+
+    seperators: Iterator[int] = find_byte_boundaries(filename)
+    if skip:
+        consume(seperators, skip)
+
+    if games is None:
+        seperators = _tqdm_boundaries(filename, seperators)
+    else:
+        seperators = islice(seperators, games + 1)
+
+    blocks = split_file(filename, seperators)
+    if games:
+        blocks = tqdm(blocks, total=games, unit="games")
+
+    for i, block in enumerate(blocks):
+        if dry_run:
+            continue
+        with open(f"{output_folder}/{str(i)}.pgn", "wb") as pgn:
+            pgn.write(block)
+
+
+if __name__ == "__main__":
+    typer.run(split)
